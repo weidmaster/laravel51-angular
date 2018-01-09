400f95a34e5f84b064d272c8e1fd72b5f3f86f0d
Eduardo Weidman Barijan <eduwb88@gmail.com>
2018-01-09 16:29:37 -0200

    Client CRUD

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use CodeProject\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        return Client::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  Request  $request
+     * @param  int  $id
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $client = Client::find($id);
+        $client->update($request->all());
+        
+        return $client;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        Client::find($id)->delete();
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('client', 'ClientController@index');
+Route::post('client', 'ClientController@store');
+Route::get('client/{id}', 'ClientController@show');
+Route::delete('client/{id}', 'ClientController@destroy');
+Route::put('client/{id}', 'ClientController@update');
