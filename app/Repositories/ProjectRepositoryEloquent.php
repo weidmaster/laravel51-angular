f10a099636109234a4604d0bedd6e9ed6712e20f
Eduardo Weidman Barijan <eduwb88@gmail.com>
2018-01-12 10:49:40 -0200

    creates Project Entity with Repository

---
+++ app/Repositories/ProjectRepositoryEloquent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CodeProject\Repositories;
+
+use Prettus\Repository\Eloquent\BaseRepository;
+
+use CodeProject\Repositories\ProjectRepository;
+use CodeProject\Entities\Project;
+
+
+/**
+ * Class ProjectRepositoryEloquent
+ * @package namespace CodeProject\Repositories;
+ */
+class ProjectRepositoryEloquent extends BaseRepository implements ProjectRepository
+{
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Project::class;
+    }
+}
